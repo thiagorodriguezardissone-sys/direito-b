9e8c07cbd7d6e8dbfb2baff39b46c842843b241c
thiagorodriguezardissone-sys <thiagorodriguezardissone@gmail.com>
2025-10-11 19:55:48 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Meu programa")
 st.write("Alô mundo")
-st.image("images/programming-background-with-person-working-with-codes-computer-1200x800")
+
 nome = st.text_input("Digite o seu nome:")
 if nome:
     st.write(nome.upper())
