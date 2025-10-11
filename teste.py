8064b32509c8e21769fafd98e51a1bf9faf315ff
thiagorodriguezardissone-sys <thiagorodriguezardissone@gmail.com>
2025-10-11 20:01:02 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,3 +6,5 @@ st.write("Alô mundo")
 nome = st.text_input("Digite o seu nome:")
 if nome:
     st.write(nome.upper())
+ if st.checkbox("Quer ver uma mensagem especial?"):
+     st.success("✨ Continue estudando e você vai longe em programação! 🚀")
