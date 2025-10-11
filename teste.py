90ae54203d0a560ef0232c21b54dee2dccb97698
thiagorodriguezardissone-sys <thiagorodriguezardissone@gmail.com>
2025-10-11 20:08:19 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,12 +7,12 @@ nome = st.text_input("Digite o seu nome:")
 if nome:
     st.write(nome.upper())
 if st.checkbox("Quer ver uma mensagem especial?"):
-     st.success("✨ Continue estudando e você vai longe em programação! 🚀")
+     st.success(" Continue estudando e você vai longe em programação! ")
 import random
 
-if st.button("🎲 Gerar meu número da sorte"):
+if st.button(" Gerar meu número da sorte"):
     numero = random.randint(1, 100)
-    st.success(f"Seu número da sorte é: {numero} 🍀")
+    st.success(f"Seu número da sorte é: {numero} ")
 ano = st.number_input("Digite seu ano de nascimento:", 1900, 2025)
 if ano:
-    st.write(f"Você tem {2025 - ano} anos. ⏳")
+    st.write(f"Você tem {2025 - ano} anos. ")
