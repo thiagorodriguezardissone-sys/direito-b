e67f5c1b875730c2989e5dec4436482a3f177ebd
thiagorodriguezardissone-sys <thiagorodriguezardissone@gmail.com>
2025-10-13 15:13:17 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+
 dataset = pd.read_csv("https://www.irdx.com.br/media/uploads/paises.csv")
 
-
-
-fig = px.scatter_geo(dataset,
-                     lat=dataset["latitude"],
-                     lon=dataset["longitude"],
+fig = px.cloropleth(dataset,
+                     locations=dataset["iso3"],
+                     color=dataset["longitude"],
                      hover_name=dataset["nome"])
-fig.update_layout(title="Coordenadas dos Países no Mapa",
-                  geo_scope= "world")
-fig.show()
+
+fig.update_layout(title="Mapa Cloroplético dos Países", geo_scope= "world")
+
+st.plotly_chart(fig,  use_container_width=True, theme= "streamlit")
