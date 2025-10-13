bc5b957e045071701e6b7c1adf8793beb6818610
thiagorodriguezardissone-sys <thiagorodriguezardissone@gmail.com>
2025-10-13 15:04:35 -0300

    Update paises.py

--- paises.py
+++ paises.py
@@ -3,4 +3,12 @@ import streamlit as st
 
 dataset = pd.read_csv("https://www.irdx.com.br/media/uploads/paises.csv")
 
-st.dataframe(dataset)
+import plotly.express as px
+
+fig = px.scatter_geo(dataset,
+                     lat=dataset["latitude"],
+                     lon=dataset["longitude"],
+                     hover_name=dataset["nome"])
+fig.update_layout(title="Coordenadas dos Países no Mapa",
+                  geo_scope= "world")
+fig.show()
