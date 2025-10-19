cf3723013803c67a8f58758d1055444fd23ab4da
thiagorodriguezardissone-sys <thiagorodriguezardissone@gmail.com>
2025-10-19 17:58:08 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import matplotlib.pyplot as plt
+
+st.title("📊 Análise de Deputados por Partido")
+
 
 df = pd.read_csv("deputados.csv")
+
 partidos = df["partido"].value_counts()
 
-plt.figure(figsize=(10,5))
-partidos.plot(kind="bar", color="skyblue")
-plt.title("Número de Deputados por Partido")
-plt.xlabel("Partido")
-plt.ylabel("Quantidade")
-plt.tight_layout()
-plt.show()
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.bar(partidos.index, partidos.values, color="cornflowerblue")
+ax.set_title("Número de Deputados por Partido")
+ax.set_xlabel("Partido")
+ax.set_ylabel("Quantidade")
+
+
+st.pyplot(fig)
+
+st.subheader("📋 Tabela de Dados")
+st.dataframe(df)
